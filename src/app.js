7f09598b7525251c9ce61087ee3ce6c2712601df
Bruno de Queirós <42852056+bqdesigner@users.noreply.github.com>
2019-09-16 14:29:33 -0300

    Loading search
    Disable and enable search at search time, show created and favorite repositories, organize proptypes and set default values.

--- src/app.js
+++ src/app.js
@@ -11,17 +11,29 @@ class App extends Component {
     this.state = {
       userinfo: null,
       repos: [],
-      starred: []
+      starred: [],
+      isFetching: false
     }
   }
+
+  // Criando função para padronizar a request na mesma URL, já com verificações caso alguns dados nãos sejam passados, como o nome do usuário e o tipo de acesso, como repos ou starred
+  getGithubApiUrl (username, type) {
+    const internalUser = username ? `/${username}` : ''
+    const internalType = type ? `/${type}` : ''
+    return `https://api.github.com/users${internalUser}${internalType}`
+  } 
 
   handleSearch(e) {
     // Pega o valor informado no search
     const value = e.target.value
     const keyCode = e.which || e.keyCode
     const ENTER = 13
+    // const target = e.target
+
     if (keyCode === ENTER) {
-      ajax().get(`https://api.github.com/users/${value}`)
+      this.setState({ isFetching: true })
+      // target.disabled = true
+      ajax().get(this.getGithubApiUrl(value))
       .then((result) => {
         // console.log(result)
         this.setState({
@@ -32,16 +44,23 @@ class App extends Component {
             repos: result.public_repos,
             followers: result.followers,
             following: result.following
-          }
+          },
+          repos: [],
+          starred: []
         })
       })
+      // .always(() => {
+      //   target.disabled = false
+      // })
+      .always(() => this.setState({ isFetching: false }))
     }
   }
 
   getRepos (type) {
     return (e) => {
       // console.log('type', type)
-      ajax().get(`https://api.github.com/users/bqdesigner/${type}`)
+      const username = this.state.userinfo.login
+      ajax().get(this.getGithubApiUrl(username, type))
       .then((result) => {
         this.setState({
           [type]: result.map((repo) => ({
@@ -58,6 +77,7 @@ class App extends Component {
       userinfo={this.state.userinfo}
       repos={this.state.repos}
       starred={this.state.starred}
+      isFetching={this.state.isFetching}
       handleSearch={(e) => this.handleSearch(e)}
       getRepos={this.getRepos('repos')}
       getStarred={this.getRepos('starred')}
